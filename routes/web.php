8bc996a4daaaa3247a645c5a8f26683f0b2eb53d
Rio Ragil Ramdani <rioragilramdani7@gamail.com>
2024-10-24 06:12:58 +0700

    Update landing page view

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('LandingPage');
 });
 Route::resource('/products', \App\Http\Controllers\ProductController::class);
