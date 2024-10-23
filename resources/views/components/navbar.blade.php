1ee59e30bcaf8b48c859d6ca5bfbd8774df4a865
Rio Ragil Ramdani <rioragilramdani7@gamail.com>
2024-10-24 06:13:33 +0700

    Add view components for Footer, Layout, and Navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,25 @@
+<style>
+    .glass-effect {
+        background: rgba(255, 255, 255, 0.2);
+        backdrop-filter: blur(10px);
+        -webkit-backdrop-filter: blur(10px);
+        border: 1px solid rgba(255, 255, 255, 0.3);
+    }
+</style>
+<div class="fixed w-full z-10 glass-effect py-4">
+    <div class="container mx-auto flex justify-between items-center">
+        <div class="flex items-center space-x-4">
+        </div>
+        <div class="flex space-x-8 text-black font-bold">
+            <a href="#" class="hover:text-green-700">HOME</a>
+            <a href="#" class="hover:text-green-700">PRODUCTS</a>
+            <a href="#" class="hover:text-green-700">SHOP</a>
+            <div class="custom-logo text-white text-2xl">O! umia</div>
+            <a href="#" class="hover:text-green-700">ABOUT US</a>
+            <a href="#" class="hover:text-green-700">FAQ</a>
+            <a href="#" class="hover:text-green-700">CONTACT US</a>
+        </div>
+        <div class="custom-cart relative">
+        </div>
+    </div>
+</div>
