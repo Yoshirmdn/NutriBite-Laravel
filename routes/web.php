9d7449e397c1adf88a8dc99f1a1417dce103d1c0
Rio Ragil Ramdani <rioragilramdani7@gamail.com>
2024-10-26 21:31:29 +0700

    Refactor routes to include BlogController and update ProductController import

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('LandingPage');
 });
-Route::resource('/products', \App\Http\Controllers\ProductController::class);
+Route::resource('/products', ProductController::class);
+Route::resource('/blogs', BlogController::class);
