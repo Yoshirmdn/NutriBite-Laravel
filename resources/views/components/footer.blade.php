372ed47b09949ee481874cfe4ab897d5d1149562
Rio Ragil Ramdani <rioragilramdani7@gamail.com>
2024-10-26 21:10:00 +0700

    Refactor footer component and add styling

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,53 @@
 <div>
-    <!-- Well begun is half done. - Aristotle -->
+    <svg class="-z-30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#451a03" fill-opacity="1"
+            d="M0,192L34.3,170.7C68.6,149,137,107,206,122.7C274.3,139,343,213,411,208C480,203,549,117,617,117.3C685.7,117,754,203,823,224C891.4,245,960,203,1029,202.7C1097.1,203,1166,245,1234,256C1302.9,267,1371,245,1406,234.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z">
+        </path>
+    </svg>
+    <footer class="bg-amber-950 text-white py-8 -mt-1">
+        <div class="container mx-auto flex justify-evenly">
+            <div class="w-1/4">
+                <h3 class="font-semibold text-lg">NutriBite</h3>
+                <p class="mt-4">Jl. Raya Bogor No. 12, Jakarta</p>
+                <p>021-1234567</p>
+                <p>
+                    <a href="mailto:info@nutribite.com" class="text-white">info@nutribite.com</a>
+                </p>
+            </div>
+            <div class="w-1/4">
+                <h3 class="font-semibold text-lg">Quick Links</h3>
+                <ul class="mt-4">
+                    <li>
+                        <a href="#" class="hover:text-blue-500">Home</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:text-blue-500">About</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:text-blue-500">Products</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:text-blue-500">Blog</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="w-1/4">
+                <h3 class="font-semibold text-lg">Follow Us</h3>
+                <ul class="mt-4">
+                    <li>
+                        <a href="#" class="hover:text-blue-500">Facebook</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:text-blue-500">Instagram</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:text-blue-500">Twitter</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:text-blue-500">LinkedIn</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
 </div>
